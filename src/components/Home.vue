d1e316fe9d060f70fe2b2aa4fca2234a731899d4
Andrea-Pinzon <71514080+Andrea-Pinzon@users.noreply.github.com>
2022-09-28 17:10:57 -0500

    detalles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,7 +2,7 @@
     <div class="home">
         <div class="informacion">
             <h1>Bienvenido al home <span>{{username}}!</span></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, impedit. Earum doloremque illo, nobis quibusdam, odio recusandae consequuntur sit temporibus impedit enim, quia eos unde doloribus eligendi repellendus? Rem nobis temporibus perferendis fugit similique perspiciatis, libero, facilis laborum quaerat voluptates aliquam dicta aspernatur a vel delectus quo. Maiores molestias nisi cumque velit! Nesciunt praesentium alias repellendus distinctio dicta odit odio quibusdam. Repudiandae optio dolorem quae ullam debitis? Maiores deleniti suscipit labore in inventore earum explicabo soluta, illo quisquam excepturi unde dicta ea natus exercitationem illum iusto, ratione eaque voluptatibus reprehenderit cumque recusandae aut numquam dolorum. Quaerat modi ad aut perspiciatis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, impedit. Earum doloremque illo, nobis quibusdam, odio recusandae consequuntur sit temporibus impedit enim, quia eos unde doloribus eligendi repellendus? Rem nobis temporibus perferendis fugit similique perspiciatis, libero, facilis laborum quaerat voluptates aliquam dicta aspernatur a vel delectus quo. Maiores molestias nisi cumque velit! Nesciunt praesentium alias repellendus distinctio dicta odit odio quibusdam.</p>
         </div>
     </div>
 </template>
