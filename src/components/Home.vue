2c8b4e77477fae36997901901c2a07e3c77a96f4
Andrea-Pinzon <71514080+Andrea-Pinzon@users.noreply.github.com>
2022-10-02 14:46:15 -0500

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
         <div class="informacion">
-            <h1>¡Bienvenido, <span>{{ username }}</span>!</h1>
+            <h1>Bienvenido, <span>{{ username }}</span>!</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, impedit. Earum doloremque illo, nobis quibusdam, odio recusandae consequuntur sit temporibus impedit enim, quia eos unde doloribus eligendi repellendus? Rem nobis temporibus perferendis fugit similique perspiciatis, libero, facilis laborum quaerat voluptates aliquam dicta aspernatur a vel delectus quo. Maiores molestias nisi cumque velit! Nesciunt praesentium alias repellendus distinctio dicta odit odio quibusdam.</p>
         </div>
     </div>
