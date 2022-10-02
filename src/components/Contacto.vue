ba3df2df0c98e4d2b827964a3ed3a7ebc65ef40f
Andrea-Pinzon <71514080+Andrea-Pinzon@users.noreply.github.com>
2022-10-02 00:36:22 -0500

    Cambios en el contactenos

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="formulario_contacto">
         <div class="info">
-            <h1>Envíe su formulario</h1>
-            <form action="https://formsubmit.co/milenitha.666666@gmail.com" method="POST">
-                <label for="Nombre">Nombre Completo</label>
+            <h2>Contactenos</h2>
+            <form action="https://formsubmit.co/9047145694742bc000b16ea1a5cad353" method="POST">
+                <label for="Nombre">Nombre Completo:</label>
                 <input type="text" name ="name">
-
-                <label for="email">Correo Electronico</label>
+                <br>
+                <label for="email">Correo Electronico:</label>
                 <input type="email" name ="email">
-
-                <label for="phone">Celular</label>
+                <br>
+                <label for="phone">Celular:</label>
                 <input type="text" name ="phone">
-
-                <label for="subject">Asunto</label>
+                <br>
+                <label for="subject">Asunto:</label>
                 <input type="text" name ="subject">
-
-                <label for="comments">Comentarios</label>
-                <textarea type="comments" cols="15" rows="5"></textarea>
-
-                <input type="submit" value="Enviar" @click="exito">
+                <br>
+                <label for="comments">Comentarios:</label>
+                <textarea type="comments" cols="10" rows="1"></textarea>
+                <br>
+                <input type="submit" value="Enviar" @click="exito" class="botton">
 
                 <input type="hidden" name="_next" value="https://class-nowapp-vue.herokuapp.com/contacto">
                 <input type="hidden" name="_captcha" value="false">
@@ -49,25 +49,67 @@ import swal from 'sweetalert'
     .formulario_contacto {
         width: 100%;
         height: 100%;
-        background-color: red;
         display: flex;
         justify-content: center;
         align-items: center;
     }
     .info {
-        width: 70%;
-        background-color: pink;
+        width: 80%;
+        height: 85%;
+        background-color: rgba(255,255,255, .5);
+        border-radius: 20px;
         display: flex;
-        flex-direction: column;
+        flex-wrap: wrap;
         justify-content: center;
         align-items: center;
-        gap: 10px;
-        text-align: center;
+        align-content: center;
     }
 
-    .info h1 {
-        width: 80%;
-        margin-left: 20px;
+    .info h2 {
+        width: 100%;
+        padding: 5px 0;
         font-family: var(--font-titulos);
+        text-align: center;
+        text-transform: uppercase;
+    }
+
+    .info form {
+        width: 90%;
+        height: 80%;
+        overflow-y: auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        gap: 8px;
+    }
+
+    .info label {
+        width: 30%;
+    }
+
+    .info input, .info textarea {
+        width: 50%;
+        padding: 10px 10px;
+        background-color: transparent;
+        border: 1px solid #000;
+        border-radius: 10px;
+    }
+
+    .botton {
+        width: 50%;
+        padding: 10px 20px;
+        background-color: transparent;
+        border: 1px solid #000;
+        border-radius: 10px;
+        font-family: var(--font-letra);
+        font-weight: bold;
+        text-transform: uppercase;
+        justify-content: flex-end;
+    }
+
+    .botton:hover {
+        background-color: var(--main-color);
+        border-color: transparent;
     }
 </style>
