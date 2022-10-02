6267d9f7d3468fc70df330bfb1d354637bc920ba
Andrea-Pinzon <71514080+Andrea-Pinzon@users.noreply.github.com>
2022-10-02 11:47:24 -0500

    Arreglando Contactenos

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -3,23 +3,13 @@
         <div class="info">
             <h2>Contactenos</h2>
             <form action="https://formsubmit.co/9047145694742bc000b16ea1a5cad353" method="POST">
-                <label for="Nombre">Nombre Completo:</label>
                 <input type="text" name ="name" placeholder="Nombre Completo" required>
-                <br>
-                <label for="email">Correo Electronico:</label>
                 <input type="email" name ="email" placeholder="Correo" required>
-                <br>
-                <label for="phone">Celular:</label>
                 <input type="text" name ="phone" placeholder="Celular" required>
-                <br>
-                <label for="subject">Asunto:</label>
                 <input type="text" name ="subject" placeholder="Asunto" required>
-                <br>
-                <label for="comments">Comentarios:</label>
                 <textarea type="comments" cols="10" rows="1" placeholder="Escriba aqui..." required></textarea>
                 <br>
                 <input type="submit" value="Enviar" @click="exito" class="botton">
-
                 <input type="hidden" name="_next" value="https://class-nowapp-vue.herokuapp.com/contacto">
                 <input type="hidden" name="_captcha" value="false">
             </form>
@@ -82,10 +72,6 @@ import swal from 'sweetalert'
         justify-content: center;
         align-items: center;
         gap: 10px;
-    }
-
-    .info label {
-        width: 30%;
     }
 
     .info input, .info textarea {
